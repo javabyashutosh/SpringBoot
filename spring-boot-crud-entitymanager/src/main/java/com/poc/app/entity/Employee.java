4a3d512197ffaad68120e483c129db38e9e13759
Ashutosh_Pandey2 <ashutosh_pandey2@dell.com>
2020-04-18 20:47:57 +0530

    commit1

---
+++ spring-boot-crud-entitymanager/src/main/java/com/poc/app/entity/Employee.java
@@ -0,0 +1,31 @@
+package com.poc.app.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.*;
+import java.util.*;
+import com.fasterxml.jackson.annotation.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Employee
+{
+    @Id
+    @GeneratedValue
+    @Column(name = "employee_id")
+    private int id;
+    @Column(name = "employee_name")
+    private String name;
+    @Column(name = "date_of_birth")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date dob;
+    @Column(name = "date_of_joining")
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date doj;
+}
