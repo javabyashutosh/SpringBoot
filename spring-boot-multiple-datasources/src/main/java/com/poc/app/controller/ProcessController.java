f7935d690d31e90e2010f04a99a0dc5200df16f1
Ashutosh Pandey <52138864+javabyashutosh@users.noreply.github.com>
2020-04-19 18:19:39 +0530

    Update ProcessController.java

--- spring-boot-multiple-datasources/src/main/java/com/poc/app/controller/ProcessController.java
+++ spring-boot-multiple-datasources/src/main/java/com/poc/app/controller/ProcessController.java
@@ -26,20 +26,31 @@ public class ProcessController {
 	private final CardService cardService;
 
 	@PostMapping("/saveMember")
-	// @ResponseStatus(code = HttpStatus.CREATED, reason = "Member Details saved successfully")
-	@ApiResponses({ @ApiResponse(code = 200, message = "Member Details saved successfully into database")})
+	//@ResponseStatus(code = HttpStatus.CREATED, reason = "Member Details saved successfully")
+	@ApiResponses({
+		@ApiResponse(code =200,message = "Member Details saved successfully into database"),
+	})
 	public Member saveMemberInfo(@RequestBody Member member) {
 		member = memberService.saveMemberInfo(member);
 		return member;
 	}
 
 	@PostMapping("/saveCard")
-	@ApiResponses({ @ApiResponse(code = 200, message = "Card Details saved successfully into database")})
-	// @ResponseStatus(code = HttpStatus.CREATED, reason = "Card Details saved successfully")
+	@ApiResponses({
+		@ApiResponse(code =200,message = "Card Details saved successfully into database")
+	})
+	//@ResponseStatus(code = HttpStatus.CREATED, reason = "Card Details saved successfully")
 	public Card saveCardInfo(@RequestBody Card card) {
 		card = cardService.saveCardInfo(card);
 		return card;
 	}
-
-
+	
+	@GetMapping("/getMembers")
+	@ApiResponses({
+		@ApiResponse(code =200,message = "Members Details fetched successfully from database")
+	})
+	public List<Member> getAllMembers(){
+		
+		return memberService.fetchAllMembers();
+	}
 }
